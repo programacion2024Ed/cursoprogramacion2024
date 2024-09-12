01abc4081681af4f15127478bdd2650966037e7d
Edwin Anticona <tijeraverde2005@yahoo.com.ar>
2024-09-12 18:00:22 -0300

    practica

--- TP22/script/script01.js
+++ TP22/script/script01.js
@@ -1,3 +1,25 @@
  document.querySelector("#btnconsola").addEventListener("click",()=>{
     console.log("click a boton1")
  })
+
+ document.querySelector("#btnalerta").addEventListener("click",()=>{
+    window.alert("click a boton cerca") 
+ })
+
+ document.querySelector("#btndocumento").addEventListener("click",()=>{
+   document.querySelector("#salidas").textContent +="click1"
+ })
+
+ document.querySelector("#btnhaceclicks").addEventListener("click",()=>{
+   acumulador++
+    document.querySelector("#salida").textContent =acumulador
+ })
+ let acumulador=0
+
+ document.querySelector("#btnvalidar").addEventListener("click",()=>{
+    let valor = document.querySelector("#letras").value
+    console.log(valor)
+
+    
+   
+ })
