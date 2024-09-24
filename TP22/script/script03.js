26beed13b49000d57fe9bd897f825593f9d7479e
Edwin Anticona <tijeraverde2005@yahoo.com.ar>
2024-09-24 17:45:32 -0300

    tp22-03

---
+++ TP22/script/script03.js
@@ -0,0 +1,27 @@
+document.querySelector("#vertical").addEventListener("click",(e) => {
+    document.getElementById("distribucion").style.flexDirection ="column";
+});
+document.querySelector("#horizontal").addEventListener("click",(e) => {
+    document.getElementById("distribucion").style.flexDirection ="row";
+});
+document.querySelector("#colorRojo").addEventListener("click",(e) => {
+    document.getElementById("color").style.backgroundColor ="red";
+});
+document.querySelector("#colorVerde").addEventListener("click",(e) => {
+    document.getElementById("color").style.backgroundColor ="green";
+});
+document.querySelector("#textoReducir").addEventListener("click",(e) => {
+    document.getElementById("texto").style.fontSize ="10px";
+});
+document.querySelector("#textoAgrandar").addEventListener("click",(e) => {
+    document.getElementById("texto").style.fontSize ="25px";
+});
+document.querySelector("#textoColor").addEventListener("change", (e) => {
+    document.getElementById("texto").style.color = e.target.value;
+});
+document.querySelector("#mostrar").addEventListener("click", () => {
+    document.getElementById("verOcultar").style.display ="flex";
+});
+document.querySelector("#ocultar").addEventListener("click", () => {
+    document.getElementById("verOcultar").style.display ="none";
+});
