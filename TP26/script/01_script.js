dfb3448d73003e5fe6dfb2f4b5c9ea353830440e
Edwin Anticona <tijeraverde2005@yahoo.com.ar>
2024-11-14 17:39:25 -0300

    TP27-1

---
+++ TP26/script/01_script.js
@@ -0,0 +1,8 @@
+document.querySelector("#timeOut").addEventListener("click", () => {
+  console.log(1);
+  console.log(2);
+  setTimeout(() => {console.log(3)}, 2000);
+  console.log(4);
+  setTimeout(() => {console.log(5)}, 2000);
+  console.log(6);
+}) 
